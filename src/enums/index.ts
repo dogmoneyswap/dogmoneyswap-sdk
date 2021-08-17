00bf8ede5425a38ab68e9e4d0a15efd163a7d0cb
Matthew Lilley <hello@matthewlilley.com>
2021-08-17 14:33:43 +0100

    feat: export fee

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -2,3 +2,4 @@ export { ChainId } from "./ChainId";
 export { Rounding } from "./Rounding";
 export { TradeType } from "./TradeType";
 export { KashiAction } from "./KashiAction";
+export { Fee } from "./Fee";
