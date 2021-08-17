b5388c6d3699926210db82b4406a593d3a8a6b71
Matthew Lilley <hello@matthewlilley.com>
2021-08-17 07:20:01 +0100

    feat: decouple weth9 & wnative tokens & addresses

--- src/entities/Token.ts
+++ src/entities/Token.ts
@@ -1,8 +1,11 @@
+import { WETH9_ADDRESS, WNATIVE_ADDRESS } from "../constants";
+
 import { AbstractCurrency } from "./AbstractCurrency";
 import { ChainId } from "../enums";
 import { Currency } from "./Currency";
 import invariant from "tiny-invariant";
 import { validateAndParseAddress } from "../functions/validateAndParseAddress";
+
 /**
  * Represents an ERC20 token with a unique address and some metadata.
  */
@@ -78,49 +81,49 @@ export function currencyEquals(
 export const WETH9: { [chainId: number]: Token } = {
   [ChainId.MAINNET]: new Token(
     ChainId.MAINNET,
-    "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    WETH9_ADDRESS[ChainId.MAINNET],
     18,
     "WETH9",
     "Wrapped Ether"
   ),
   [ChainId.ROPSTEN]: new Token(
     ChainId.ROPSTEN,
-    "0xc778417E063141139Fce010982780140Aa0cD5Ab",
+    WETH9_ADDRESS[ChainId.ROPSTEN],
     18,
     "WETH9",
     "Wrapped Ether"
   ),
   [ChainId.RINKEBY]: new Token(
     ChainId.RINKEBY,
-    "0xc778417E063141139Fce010982780140Aa0cD5Ab",
+    WETH9_ADDRESS[ChainId.RINKEBY],
     18,
     "WETH9",
     "Wrapped Ether"
   ),
   [ChainId.GÖRLI]: new Token(
     ChainId.GÖRLI,
-    "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6",
+    WETH9_ADDRESS[ChainId.GÖRLI],
     18,
     "WETH9",
     "Wrapped Ether"
   ),
   [ChainId.KOVAN]: new Token(
     ChainId.KOVAN,
-    "0xd0A1E359811322d97991E03f863a0C30C2cF029C",
+    WETH9_ADDRESS[ChainId.KOVAN],
     18,
     "WETH9",
     "Wrapped Ether"
   ),
   [ChainId.ARBITRUM]: new Token(
     ChainId.ARBITRUM,
-    "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+    WETH9_ADDRESS[ChainId.ARBITRUM],
     18,
     "WETH9",
     "Wrapped Ether"
   ),
   [ChainId.ARBITRUM_TESTNET]: new Token(
     ChainId.ARBITRUM_TESTNET,
-    "0xf8456e5e6A225C2C1D74D8C9a4cB2B1d5dc1153b",
+    WETH9_ADDRESS[ChainId.ARBITRUM_TESTNET],
     18,
     "WETH",
     "Wrapped Ether"
@@ -128,7 +131,7 @@ export const WETH9: { [chainId: number]: Token } = {
 
   [ChainId.BSC]: new Token(
     ChainId.BSC,
-    "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+    WETH9_ADDRESS[ChainId.BSC],
     18,
     "WETH",
     "Wrapped Ether"
@@ -136,7 +139,7 @@ export const WETH9: { [chainId: number]: Token } = {
 
   [ChainId.FANTOM]: new Token(
     ChainId.FANTOM,
-    "0x74b23882a30290451A17c44f4F05243b6b58C76d",
+    WETH9_ADDRESS[ChainId.FANTOM],
     18,
     "WETH",
     "Wrapped Ether"
@@ -144,7 +147,7 @@ export const WETH9: { [chainId: number]: Token } = {
 
   [ChainId.MATIC]: new Token(
     ChainId.MATIC,
-    "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
+    WETH9_ADDRESS[ChainId.MATIC],
     18,
     "WETH",
     "Wrapped Ether"
@@ -152,7 +155,7 @@ export const WETH9: { [chainId: number]: Token } = {
 
   [ChainId.OKEX]: new Token(
     ChainId.OKEX,
-    "0x2170Ed0880ac9A755fd29B2688956BD959F933F8",
+    WETH9_ADDRESS[ChainId.OKEX],
     18,
     "WETH",
     "Wrapped Ether"
@@ -160,7 +163,7 @@ export const WETH9: { [chainId: number]: Token } = {
 
   [ChainId.HECO]: new Token(
     ChainId.HECO,
-    "0x64FF637fB478863B7468bc97D30a5bF3A428a1fD",
+    WETH9_ADDRESS[ChainId.HECO],
     18,
     "WETH",
     "Wrapped Ether"
@@ -168,7 +171,7 @@ export const WETH9: { [chainId: number]: Token } = {
 
   [ChainId.HARMONY]: new Token(
     ChainId.HARMONY,
-    "0x6983D1E6DEf3690C4d616b13597A09e6193EA013",
+    WETH9_ADDRESS[ChainId.HARMONY],
     18,
     "WETH",
     "Wrapped Ether"
@@ -176,7 +179,7 @@ export const WETH9: { [chainId: number]: Token } = {
 
   [ChainId.XDAI]: new Token(
     ChainId.XDAI,
-    "0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1",
+    WETH9_ADDRESS[ChainId.XDAI],
     18,
     "WETH",
     "Wrapped Ether"
@@ -184,7 +187,7 @@ export const WETH9: { [chainId: number]: Token } = {
 
   [ChainId.AVALANCHE]: new Token(
     ChainId.AVALANCHE,
-    "0xf20d962a6c8f70c731bd838a3a388D7d48fA6e15",
+    WETH9_ADDRESS[ChainId.AVALANCHE],
     18,
     "WETH",
     "Wrapped Ether"
@@ -199,49 +202,49 @@ export const WNATIVE: { [chainId: number]: Token } = {
   [ChainId.KOVAN]: WETH9[ChainId.KOVAN],
   [ChainId.FANTOM]: new Token(
     ChainId.FANTOM,
-    "0x21be370D5312f44cB42ce377BC9b8a0cEF1A4C83",
+    WNATIVE_ADDRESS[ChainId.FANTOM],
     18,
     "WFTM",
     "Wrapped FTM"
   ),
   [ChainId.FANTOM_TESTNET]: new Token(
     ChainId.FANTOM_TESTNET,
-    "0xf1277d1Ed8AD466beddF92ef448A132661956621",
+    WNATIVE_ADDRESS[ChainId.FANTOM_TESTNET],
     18,
     "FTM",
     "Wrapped FTM"
   ),
   [ChainId.MATIC]: new Token(
     ChainId.MATIC,
-    "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270",
+    WNATIVE_ADDRESS[ChainId.MATIC],
     18,
     "WMATIC",
     "Wrapped Matic"
   ),
   [ChainId.MATIC_TESTNET]: new Token(
     ChainId.MATIC_TESTNET,
-    "0x5B67676a984807a212b1c59eBFc9B3568a474F0a",
+    WNATIVE_ADDRESS[ChainId.MATIC_TESTNET],
     18,
     "WMATIC",
     "Wrapped Matic"
   ),
   [ChainId.XDAI]: new Token(
     ChainId.XDAI,
-    "0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d",
+    WNATIVE_ADDRESS[ChainId.XDAI],
     18,
     "WXDAI",
     "Wrapped xDai"
   ),
   [ChainId.BSC]: new Token(
     ChainId.BSC,
-    "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    WNATIVE_ADDRESS[ChainId.BSC],
     18,
     "WBNB",
     "Wrapped BNB"
   ),
   [ChainId.BSC_TESTNET]: new Token(
     ChainId.BSC_TESTNET,
-    "0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd",
+    WNATIVE_ADDRESS[ChainId.BSC_TESTNET],
     18,
     "WBNB",
     "Wrapped BNB"
@@ -250,77 +253,77 @@ export const WNATIVE: { [chainId: number]: Token } = {
   [ChainId.ARBITRUM_TESTNET]: WETH9[ChainId.ARBITRUM_TESTNET],
   [ChainId.MOONBEAM_TESTNET]: new Token(
     ChainId.MOONBEAM_TESTNET,
-    "0xe73763DB808ecCDC0E36bC8E32510ED126910394",
+    WNATIVE_ADDRESS[ChainId.MOONBEAM_TESTNET],
     18,
     "WETH",
     "Wrapped Ether"
   ),
   [ChainId.AVALANCHE]: new Token(
     ChainId.AVALANCHE,
-    "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7",
+    WNATIVE_ADDRESS[ChainId.AVALANCHE],
     18,
     "WAVAX",
     "Wrapped AVAX"
   ),
   [ChainId.AVALANCHE_TESTNET]: new Token(
     ChainId.AVALANCHE_TESTNET,
-    "0xd00ae08403B9bbb9124bB305C09058E32C39A48c",
+    WNATIVE_ADDRESS[ChainId.AVALANCHE_TESTNET],
     18,
     "WAVAX",
     "Wrapped AVAX"
   ),
   [ChainId.HECO]: new Token(
     ChainId.HECO,
-    "0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F",
+    WNATIVE_ADDRESS[ChainId.HECO],
     18,
     "WHT",
     "Wrapped HT"
   ),
   [ChainId.HECO_TESTNET]: new Token(
     ChainId.HECO_TESTNET,
-    "0x5B2DA6F42CA09C77D577a12BeaD0446148830687",
+    WNATIVE_ADDRESS[ChainId.HECO_TESTNET],
     18,
     "WHT",
     "Wrapped HT"
   ),
   [ChainId.HARMONY]: new Token(
     ChainId.HARMONY,
-    "0xcF664087a5bB0237a0BAd6742852ec6c8d69A27a",
+    WNATIVE_ADDRESS[ChainId.HARMONY],
     18,
     "WONE",
     "Wrapped ONE"
   ),
   [ChainId.HARMONY_TESTNET]: new Token(
     ChainId.HARMONY_TESTNET,
-    "0x7a2afac38517d512E55C0bCe3b6805c10a04D60F",
+    WNATIVE_ADDRESS[ChainId.HARMONY_TESTNET],
     18,
     "WONE",
     "Wrapped ONE"
   ),
   [ChainId.OKEX]: new Token(
     ChainId.OKEX,
-    "0x8F8526dbfd6E38E3D8307702cA8469Bae6C56C15",
+    WNATIVE_ADDRESS[ChainId.OKEX],
     18,
     "WOKT",
     "Wrapped OKExChain"
   ),
   [ChainId.OKEX_TESTNET]: new Token(
     ChainId.OKEX_TESTNET,
-    "0x2219845942d28716c0F7C605765fABDcA1a7d9E0",
+    WNATIVE_ADDRESS[ChainId.OKEX_TESTNET],
     18,
     "WOKT",
     "Wrapped OKExChain"
   ),
   [ChainId.CELO]: new Token(
     ChainId.CELO,
-    "0x471EcE3750Da237f93B8E339c536989b8978a438",
+    WNATIVE_ADDRESS[ChainId.CELO],
     18,
     "CELO",
     "Celo"
   ),
   [ChainId.PALM]: new Token(
     ChainId.PALM,
-    "0xF98cABF0a963452C5536330408B2590567611a71",
+    WNATIVE_ADDRESS[ChainId.PALM],
     18,
     "WPALM",
     "Wrapped Palm"
