511440dc3a8b247da0aee2706c69809e8cf7ce0c
Matthew Lilley <hello@matthewlilley.com>
2021-08-17 02:50:52 +0100

    style: prettier format

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,39 +1,41 @@
-export * from './addresses'
-export * from './natives'
+export * from "./addresses";
+export * from "./natives";
 
-import JSBI from 'jsbi'
+import JSBI from "jsbi";
 
 export const MaxUint256 = JSBI.BigInt(
-  '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
-)
+  "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+);
 
 export const INIT_CODE_HASH: string =
-  '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303'
+  "0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303";
 
-export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000);
 
 // exports for internal consumption
-export const ZERO = JSBI.BigInt(0)
-export const ONE = JSBI.BigInt(1)
-export const TWO = JSBI.BigInt(2)
-export const THREE = JSBI.BigInt(3)
-export const FIVE = JSBI.BigInt(5)
-export const TEN = JSBI.BigInt(10)
-export const _100 = JSBI.BigInt(100)
-export const _997 = JSBI.BigInt(997)
-export const _1000 = JSBI.BigInt(1000)
+export const ZERO = JSBI.BigInt(0);
+export const ONE = JSBI.BigInt(1);
+export const TWO = JSBI.BigInt(2);
+export const THREE = JSBI.BigInt(3);
+export const FIVE = JSBI.BigInt(5);
+export const TEN = JSBI.BigInt(10);
+export const _100 = JSBI.BigInt(100);
+export const _997 = JSBI.BigInt(997);
+export const _1000 = JSBI.BigInt(1000);
 
 export enum SolidityType {
-  uint8 = 'uint8',
-  uint256 = 'uint256',
+  uint8 = "uint8",
+  uint256 = "uint256"
 }
 
 export const SOLIDITY_TYPE_MAXIMA = {
-  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint8]: JSBI.BigInt("0xff"),
   [SolidityType.uint256]: JSBI.BigInt(
-    '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'
-  ),
-}
+    "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+  )
+};
 
-export const LAMBDA_URL = 'https://9epjsvomc4.execute-api.us-east-1.amazonaws.com/dev';
-export const SOCKET_URL = 'wss://hfimt374ge.execute-api.us-east-1.amazonaws.com/dev';
+export const LAMBDA_URL =
+  "https://9epjsvomc4.execute-api.us-east-1.amazonaws.com/dev";
+export const SOCKET_URL =
+  "wss://hfimt374ge.execute-api.us-east-1.amazonaws.com/dev";
--- src/types/MultiRouterTypes.ts
+++ src/types/MultiRouterTypes.ts
@@ -1,27 +1,27 @@
-import {BigNumber} from '@ethersproject/bignumber'
+import { BigNumber } from '@ethersproject/bignumber'
 
 export interface RToken {
-    name: string;
-    gasPrice: number;
+  name: string
+  gasPrice: number
 }
 
 export enum PoolType {
-    ConstantProduct = 'ConstantProduct',
-    Weighted = 'Weighted',
-    Hybrid = 'Hybrid',
-    ConcentratedLiquidity = 'ConcentratedLiquidity'
+  ConstantProduct = 'ConstantProduct',
+  Weighted = 'Weighted',
+  Hybrid = 'Hybrid',
+  ConcentratedLiquidity = 'ConcentratedLiquidity',
 }
 
 export interface PoolInfo {
-    address: string;
-    token0: RToken;
-    token1: RToken;
-    type: PoolType;
-    reserve0: BigNumber;
-    reserve1: BigNumber;
-    fee: number;
-    minLiquidity: number;
-    swapGasCost: number;
+  address: string
+  token0: RToken
+  token1: RToken
+  type: PoolType
+  reserve0: BigNumber
+  reserve1: BigNumber
+  fee: number
+  minLiquidity: number
+  swapGasCost: number
 }
 
 type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
@@ -29,84 +29,84 @@ type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>
 type PoolInfoWithDefaults = PartialBy<PoolInfo, 'minLiquidity' | 'swapGasCost'>
 
 export class Pool {
-    address: string;
-    token0: RToken;
-    token1: RToken;
-    type: PoolType;
-    reserve0: BigNumber;
-    reserve1: BigNumber;
-    fee: number;
-    minLiquidity: number;
-    swapGasCost: number;
+  address: string
+  token0: RToken
+  token1: RToken
+  type: PoolType
+  reserve0: BigNumber
+  reserve1: BigNumber
+  fee: number
+  minLiquidity: number
+  swapGasCost: number
 
-    constructor(_info:PoolInfoWithDefaults) {
-        const info = {
-            minLiquidity: 1000,
-            swapGasCost: 40_000,
-            ..._info
-        }
-        this.address = info.address;
-        this.token0 = info.token0;
-        this.token1 = info.token1;
-        this.type = info.type;
-        this.reserve0 = info.reserve0;
-        this.reserve1 = info.reserve1;
-        this.fee = info.fee;
-        this.minLiquidity = info.minLiquidity;
-        this.swapGasCost = info.swapGasCost;
+  constructor(_info: PoolInfoWithDefaults) {
+    const info = {
+      minLiquidity: 1000,
+      swapGasCost: 40_000,
+      ..._info,
     }
+    this.address = info.address
+    this.token0 = info.token0
+    this.token1 = info.token1
+    this.type = info.type
+    this.reserve0 = info.reserve0
+    this.reserve1 = info.reserve1
+    this.fee = info.fee
+    this.minLiquidity = info.minLiquidity
+    this.swapGasCost = info.swapGasCost
+  }
 }
 
-type PoolInfoNoType = Omit<PoolInfoWithDefaults, "type">;
+type PoolInfoNoType = Omit<PoolInfoWithDefaults, 'type'>
 
 export class ConstantProductPool extends Pool {
-    constructor(info: PoolInfoNoType) {
-        super({
-            type: PoolType.ConstantProduct,
-            ...info
-        })
-    }
+  constructor(info: PoolInfoNoType) {
+    super({
+      type: PoolType.ConstantProduct,
+      ...info,
+    })
+  }
 }
 
-type HybridPoolInfo = PoolInfoNoType & {A: number};
+type HybridPoolInfo = PoolInfoNoType & { A: number }
 
 export class HybridPool extends Pool {
-    A: number;
-    constructor(info: HybridPoolInfo) {
-        super({
-            type: PoolType.Hybrid,
-            ...info
-        })
-        this.A = info.A;
-    }
+  A: number
+  constructor(info: HybridPoolInfo) {
+    super({
+      type: PoolType.Hybrid,
+      ...info,
+    })
+    this.A = info.A
+  }
 }
 
-type WeightedPoolInfo = PoolInfoNoType & {weight0: number, weight1: number};
+type WeightedPoolInfo = PoolInfoNoType & { weight0: number; weight1: number }
 
 export class WeightedPool extends Pool {
-    weight0: number;
-    weight1: number;
-    constructor(info: WeightedPoolInfo) {
-        super({
-            type: PoolType.Weighted,
-            ...info
-        })
-        this.weight0 = info.weight0;
-        this.weight1 = info.weight1;
-    }
+  weight0: number
+  weight1: number
+  constructor(info: WeightedPoolInfo) {
+    super({
+      type: PoolType.Weighted,
+      ...info,
+    })
+    this.weight0 = info.weight0
+    this.weight1 = info.weight1
+  }
 }
 
 export interface RouteLeg {
-    address: string;
-    token: RToken;
-    swapPortion: number;        // For router contract
-    absolutePortion: number;    // To depict at webpage for user
+  address: string
+  token: RToken
+  swapPortion: number // For router contract
+  absolutePortion: number // To depict at webpage for user
 }
 
 export interface MultiRoute {
-    amountIn: number,
-    amountOut: number,
-    legs: RouteLeg[],
-    gasSpent: number,
-    totalAmountOut: number
+  amountIn: number
+  amountOut: number
+  legs: RouteLeg[]
+  gasSpent: number
+  totalAmountOut: number
 }
