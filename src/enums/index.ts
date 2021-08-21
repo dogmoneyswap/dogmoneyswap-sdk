f17e06bf68aca9c056a86e474b1c7b0dde0bf7e3
Matthew Lilley <hello@matthewlilley.com>
2021-08-21 05:17:15 +0100

    fix: add missing order status enum

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -3,3 +3,4 @@ export { Rounding } from "./Rounding";
 export { TradeType } from "./TradeType";
 export { KashiAction } from "./KashiAction";
 export { Fee } from "./Fee";
+export { OrderStatus } from "./OrderStatus";
