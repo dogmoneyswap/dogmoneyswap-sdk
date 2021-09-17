a179be0347622c19621891c753526a0298a52367
Kasumi <kasumi.null@yandex.com>
2021-09-17 16:18:28 -0500

    chore: update addresses

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -5,12 +5,12 @@ import { Token } from '../entities/Token'
 import { TokenMap } from '../types/TokenMap'
 
 export const FLEXUSD: TokenMap = {
-  [ChainId.SMARTBCH]: new Token(ChainId.SMARTBCH, FLEXUSD_ADDRESS[ChainId.SMARTBCH], 6, 'flexUSD', 'FlexUSD')
+  [ChainId.SMARTBCH]: new Token(ChainId.SMARTBCH, FLEXUSD_ADDRESS[ChainId.SMARTBCH], 18, 'flexUSD', 'flexUSD')
 }
 
 export const WETH9: TokenMap = {
   [ChainId.SMARTBCH]: new Token(ChainId.SMARTBCH, WETH9_ADDRESS[ChainId.SMARTBCH], 18, 'BCH', 'Bitcoin Cash'),
-  [ChainId.SMARTBCH_AMBER]: new Token(ChainId.SMARTBCH, WETH9_ADDRESS[ChainId.SMARTBCH], 18, 'TBCH', 'Bitcoin Cash')
+  [ChainId.SMARTBCH_AMBER]: new Token(ChainId.SMARTBCH, WETH9_ADDRESS[ChainId.SMARTBCH_AMBER], 18, 'TBCH', 'Bitcoin Cash')
 }
 
 export const WNATIVE: TokenMap = {
